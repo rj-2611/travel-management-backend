609a2355181186edd0f634b34730e0c09fc425d5
687415 <687415@host.docker.internal>
2019-10-11 17:56:02 +0530

    security

--- src/main/java/com/s2/travelmgt/controller/TripController.java
+++ src/main/java/com/s2/travelmgt/controller/TripController.java
@@ -5,12 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.Base64;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.s2.travelmgt.model.TripDetails;
+import com.s2.travelmgt.model.User;
 import com.s2.travelmgt.repository.TripInfoRepo;
 
 @RestController
@@ -31,5 +38,16 @@ public class TripController {
         List<TripDetails> tripInfo = (List<TripDetails>) tripInfoRepo.findAll();
         return tripInfo;
     }
+	
+	@RequestMapping("/login")
+	public boolean login(@RequestBody User user) {
+		return user.getUserName().equals("user") && user.getPassword().equals("password");
+	}
+
+	@RequestMapping("/user")
+	public Principal user(HttpServletRequest request) {
+		String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
+		return () -> new String(Base64.getDecoder().decode(authToken)).split(":")[0];
+	}
 }
 
